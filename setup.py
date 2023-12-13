f30e2d6e45e69e635195c3ded1c31aacd735d37e
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2023-12-13 12:12:02 -0500

    Added setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name="brainimagelibrary",
+    version="0.1",
+    description="Brain Image Library API",
+    url="https://github.com/brain-image-library/",
+    author="Ivan Cao-Berg",
+    author_email="icaoberg@psc.edu",
+    install_requires=[
+        "pandas",
+        "requests",
+        "tabulate",
+        "python-magic",
+        "humanize",
+    ],
+    packages=["brainimagelibrary"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
