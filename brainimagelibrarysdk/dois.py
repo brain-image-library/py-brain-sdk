25cf6c133c455a8a6f7aae1a4f847f96a6b5a708
Ivan E. Cao-Berg <icaoberg@cmu.edu>
2024-02-01 17:58:31 -0500

    Refactored code and fixed some bugs

--- brainimagelibrarysdk/dois.py
+++ brainimagelibrarysdk/dois.py
@@ -1,30 +1,14 @@
 import requests
-from scholarly import scholarly
-
 
 def get_number_of_citations(dataset_id="act-bag"):
     data = {}
     data["datacite"] = __get_number_of_citations_from_datacite(dataset_id=dataset_id)
-    data["gscholar"] = __get_number_of_citations_from_gscholar(dataset_id=dataset_id)
 
     return data
 
 
 def get_metadata(dataset_id="act-bag"):
     return __get_datacite_metadata(dataset_id=dataset_id)
-
-
-def __get_number_of_citations_from_gscholar(dataset_id="act-bag"):
-    brain = "10.35077"
-    query = f"{brain}/{dataset_id}"
-
-    try:
-        search_query = scholarly.search_pubs(query)
-        metadata = next(search_query)
-        return metadata["num_citations"]
-    except:
-        return None
-
 
 def __get_number_of_citations_from_datacite(dataset_id="act-bag"):
     metadata = __get_datacite_metadata(dataset_id=dataset_id)
--- setup.py
+++ setup.py
@@ -1,25 +1,36 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name="brainimagelibrary",
-    version="0.1",
-    description="Brain Image Library API",
-    url="https://github.com/brain-image-library/",
+    name="brainimagelibrarysdk",
+    version="1.0b0",  # Beta release version
+    description="Brain Image Library SDK",
+    long_description="Brain Image Library SDK is a Python library for interacting with the Brain Image Library APIs and services.",
+    long_description_content_type="text/markdown",
+    url="https://github.com/brain-image-library/py-brain-sdk",
     author="Ivan Cao-Berg",
     author_email="icaoberg@psc.edu",
+    license="GPLv3",
+    packages=find_packages(),
     install_requires=[
-        "pandas",
-        "requests",
-        "tabulate",
-        "python-magic",
-	"tqdm",
-        "scholarly",
+        "pandas>=2.2.0",
+        "requests==2.26.0",
+        "setuptools==68.2.2",
+        "tqdm==4.66.1",
         "humanize",
     ],
-    packages=["brainimagelibrary"],
+    python_requires=">=3.6",
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Medical Science Apps.",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
+    project_urls={
+        "Documentation": "https://github.com/brain-image-library/py-brain-sdk/docs",
+        "Source": "https://github.com/brain-image-library/py-brain-sdk",
+        "Tracker": "https://github.com/brain-image-library/py-brain-sdk/issues",
+    },
 )
