90661360a93396d1cac13d90acf0bcedc21ebb58
icaoberg <icaoberg@cmu.edu>
2025-03-05 03:38:01 -0500

    Added new method

--- brainimagelibrary/metadata.py
+++ brainimagelibrary/metadata.py
@@ -1,5 +1,23 @@
 import requests
 
+def by_affiliation(affiliation, params=None, headers=None):
+    api_url = f"https://api.brainimagelibrary.org/query/contributors?affiliation=Broad{affiliation}"
+
+    try:
+        response = requests.get(api_url, params=params, headers=headers)
+
+        response = response.json()
+        if (
+            "message" in response.keys()
+            and response["message"] == "GET failure, no entry found"
+        ):
+            return {}
+        else:
+            return response
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error making API request: {e}")
+        return None
 
 def retrieve(dataset_id, params=None, headers=None):
     api_url = f"https://api.brainimagelibrary.org/retrieve?bildid={dataset_id}"
