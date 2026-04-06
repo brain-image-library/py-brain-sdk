36ba1c617d19ab0ad684c8f20b2d3bd88bec03c8
Ivan Cao-Berg <icaoberg@psc.edu>
2026-04-06 18:45:26 -0400

    Update GitHub Action

--- brainimagelibrary/__init__.py
+++ brainimagelibrary/__init__.py
@@ -2,11 +2,12 @@
 brainimagelibrary
 """
 
-__version__ = "1.0"
+__version__ = "0.0.13"
 __author__ = "Ivan Cao-Berg"
 __credits__ = "Brain Image Library Team"
 
 from .retrieve import *
+from .query import *
 from .metadata import *
 from .reports import *
 from .inventory import *
